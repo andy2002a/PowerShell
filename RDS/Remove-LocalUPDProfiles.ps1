7d60fa2f36b1b26d2cc75af1cd32dd5cfcbbd00c
andy2002a <39770126+andy2002a@users.noreply.github.com>
2019-03-15 11:56:11 -0400

    Update Remove-LocalUPDProfiles.ps1

--- RDS/Remove-LocalUPDProfiles.ps1
+++ RDS/Remove-LocalUPDProfiles.ps1
@@ -3,6 +3,7 @@
 This script deletes local profile data on an RDS server that has UPDs. When UPDs are enabled user files will sometimes be left behind. This can cause some unexpected resuults with applications.
 
 The script has a failsafe that does not run if UPDs become disabled at any point.
+Andy Morales
 #>
 
 if((Get-ItemProperty 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Terminal Server\ClusterSettings').UvhdEnabled -eq 1){
