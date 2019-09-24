fe5697d24229dda20d691b90f3a4e50a923d5b2f
andy2002a <39770126+andy2002a@users.noreply.github.com>
2019-09-24 10:48:59 -0400

    Update Compact-UPDs.ps1

--- RDS/Compact-UPDs.ps1
+++ RDS/Compact-UPDs.ps1
@@ -2,11 +2,22 @@
 #Using the UNC path instead of the local path ensures that the UPDs are compacted even in the event that the local disk is changed.
 #Andy Morales
 
-$AllUPDs = Get-ChildItem \\SERVER\FSLogix -Recurse -Filter *.vhdx |where name -ne UVHD-template.vhdx | Select-Object -ExpandProperty fullname
+$VHDXPaths = @(
+    '\\SERVER\FSLogix',
+    '\\SERVER\ProfileDisks'
+)
 
-foreach ($UPD in $AllUPDs){
-    NEW-ITEM -Name compact.txt -ItemType file -force | OUT-NULL
-    ADD-CONTENT -Path compact.txt "select vdisk file= $UPD"
-    ADD-CONTENT -Path compact.txt "compact vdisk"
-    DISKPART /S compact.TXT
+$VHDXExclusions = @(
+    'UVHD-template.vhdx'
+)
+
+Foreach ($Path in $VHDXPaths){
+    $AllUPDs = Get-ChildItem $Path -Recurse -Filter *.vhdx | Where-Object {$VHDXExclusions -NotContains $_.name} | Select-Object -ExpandProperty fullname
+
+    foreach ($UPD in $AllUPDs){
+        NEW-ITEM -Name compact.txt -ItemType file -force | OUT-NULL
+        ADD-CONTENT -Path compact.txt "select vdisk file= $UPD"
+        ADD-CONTENT -Path compact.txt "compact vdisk"
+        DISKPART /S compact.TXT
+    }
 }
