943ce76757ace0d3dee4d8270c98e8114aac2dec
andy2002a <39770126+andy2002a@users.noreply.github.com>
2019-10-28 15:04:06 -0400

    Create Test-RegKeyExists.ps1

---
+++ Function/Test-RegKeyExists.ps1
@@ -0,0 +1,37 @@
+function Test-RegistryValue {
+    #https://www.jonathanmedd.net/2014/02/testing-for-the-presence-of-a-registry-key-and-value.html
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true,
+            Position = 1,
+			HelpMessage = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM')]
+		[ValidatePattern('Registry::.*')]
+        [ValidateNotNullOrEmpty()]
+        [String]$Path,
+
+        [parameter(Mandatory = $true,
+            Position = 2)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
+
+        [parameter(Position = 3)]
+        [ValidateNotNullOrEmpty()]$ValueData
+    )
+    try {
+        if ($ValueData) {
+            if ((Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop) -eq $ValueData) {
+                return $true
+            }
+            else {
+                return $false
+            }
+        }
+        else {
+            Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop | Out-Null
+            return $true
+        }
+    }
+    catch {
+        return $false
+    }
+}
