5d9bc38dc9f2a9e929c89e35179f161f96f3cf9c
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-05-14 16:47:05 -0400

    Update Test-RegKeyExists.ps1
    fixed issue with false negatives
    removed Registry:: requirement

--- Functions/Test-RegKeyExists.ps1
+++ Functions/Test-RegKeyExists.ps1
@@ -1,11 +1,19 @@
 function Test-RegistryValue {
+    <#
+    Checks if a reg key/value exists
+
+    #Modified version of the function below
     #https://www.jonathanmedd.net/2014/02/testing-for-the-presence-of-a-registry-key-and-value.html
+
+    Andy Morales
+    #>
+
     [CmdletBinding()]
     param (
         [parameter(Mandatory = $true,
             Position = 1,
-			HelpMessage = 'Registry::HKEY_LOCAL_MACHINE\SYSTEM')]
-		[ValidatePattern('Registry::.*')]
+            HelpMessage = 'HKEY_LOCAL_MACHINE\SYSTEM')]
+        [ValidatePattern('Registry::.*|HKEY_')]
         [ValidateNotNullOrEmpty()]
         [String]$Path,
 
@@ -17,7 +25,14 @@ function Test-RegistryValue {
         [parameter(Position = 3)]
         [ValidateNotNullOrEmpty()]$ValueData
     )
+
+    #Add Regdrive if it is not present
+    if ($Path -notmatch 'Registry::.*'){
+        $Path = 'Registry::' + $Path
+    }
+
     try {
+        #Reg key with value
         if ($ValueData) {
             if ((Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop) -eq $ValueData) {
                 return $true
@@ -26,9 +41,16 @@ function Test-RegistryValue {
                 return $false
             }
         }
+        #Key key without value
         else {
-            Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop | Out-Null
-            return $true
+            $RegKeyCheck = Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop
+            if ($null -eq $RegKeyCheck) {
+                #if the Key Check returns null then it probably means that the key does not exist.
+                return $false
+            }
+            else {
+                return $true
+            }
         }
     }
     catch {
